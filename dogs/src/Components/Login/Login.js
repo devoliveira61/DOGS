899d4475b264cfed6b26d08125f587863c29b408
Daniel Oliveira <devdoliveiradd@gmail.com>
2022-07-29 21:19:34 -0300

    Criando os Componentes e requisição com API

--- dogs/src/Components/Login/Login.js
+++ dogs/src/Components/Login/Login.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import LoginForm from './LoginForm';
+import LoginCreate from './LoginCreate';
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+import { Routes, Route } from 'react-router-dom';
 
 const Login = () => {
   return (
     <div>
+      <Routes>
+        <Route path="/" element={<LoginForm/>} />
+        <Route path="criar" element={<LoginCreate/>} />
+        <Route path="perdeu" element={<LoginPasswordLost/>} />
+        <Route path="resetar" element={<LoginPasswordReset/>} />
+      </Routes>
     </div>
   );
 }
---
+++ dogs/src/Components/Login/LoginCreate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginCreate = () => {
+  return (
+    <div>
+      Login Criar
+      
+    </div>
+  );
+}
+
+export default LoginCreate;
